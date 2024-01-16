9c454722309e76d151ee9908748e0bd1d6ff2630
Facundo <facundovs84@hotmail.com.ar>
2024-01-16 17:15:19 -0300

    Creadas las controladoras

---
+++ src/main/java/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,12 @@
+package persistencia;
+
+public class ControladoraPersistencia {
+    HorarioJpaController horaJpa = new HorarioJpaController();
+    OdontologoJpaController odontoJpa = new OdontologoJpaController();
+    PacienteJpaController pacJpa = new PacienteJpaController();
+    PersonaJpaController persJpa = new PersonaJpaController();
+    ResponsableJpaController respJpa = new ResponsableJpaController();
+    SecretarioJpaController secreJpa = new SecretarioJpaController();
+    TurnoJpaController turnJpa = new TurnoJpaController();
+    UsuarioJpaController usuJpa = new UsuarioJpaController();
+}
